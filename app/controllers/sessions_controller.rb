e149e7d75b48d76adf38b54d63ada2a32eabfa13
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-25 17:24:09 -0400

    Done 2.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
 
 	def create
 		# byebug
-		if @user = User.find_by(name: params[:user][:name]) && @user.authenticate(params[:user][:password])
+		@user = User.find_by(name: params[:user][:name])
+		if @user.authenticate(params[:user][:password])
 			session[:user_id] = @user.id
 			redirect_to user_path(@user)
 		else
